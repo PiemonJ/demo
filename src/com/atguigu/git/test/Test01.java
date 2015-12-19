1726789992a550d4cf03bd1404eba714e4d13b1f
zzyybs <zzyybs@126.com>
2015-12-19 20:10:15 +0800

    update 05

--- src/com/atguigu/git/test/Test01.java
+++ src/com/atguigu/git/test/Test01.java
@@ -4,9 +4,6 @@ public class Test01
 {
 	public static void main(String[] args)
 	{
-		System.out.println("hello github");
-		System.out.println("github收到来自0423的请求");
-		System.out.println("hello github2");
-		System.out.println("github收到来自0423的请求2");
+
 	}
 }
